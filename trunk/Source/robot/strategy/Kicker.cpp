d5ffe0601e1c7f7a5a251b52a46e0f01698cd7bf
muys.pascal@gmail.com <muys.pascal@gmail.com@1567ca2d-1607-d5b9-1ed5-aef1da3578f5>
2013-02-21 16:07:56 +0000

    test eclipse

---
+++ trunk/Source/robot/strategy/Kicker.cpp
@@ -0,0 +1,24 @@
+/*
+ * Kicker.cpp
+ *
+ *  Created on: 21-feb.-2013
+ *      Author: Pascal Muys
+ */
+
+#include "Kicker.h"
+
+Kicker::Kicker() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Kicker::~Kicker() {
+	// TODO Auto-generated destructor stub
+}
+
+public:
+	void run()
+	{
+		cout << endl << "Ik ga trappen!" << endl;
+	}
+
---
+++ trunk/Source/robot/strategy/Kicker.h
@@ -0,0 +1,17 @@
+/*
+ * Kicker.h
+ *
+ *  Created on: 21-feb.-2013
+ *      Author: Pascal Muys
+ */
+
+#ifndef KICKER_H_
+#define KICKER_H_
+
+class Kicker {
+public:
+	Kicker();
+	virtual ~Kicker();
+};
+
+#endif /* KICKER_H_ */
