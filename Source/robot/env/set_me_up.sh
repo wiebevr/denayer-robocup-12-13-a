79fdf022fab4ba4a28c561a6d54b64f0521ab855
wiebevr <wiebevr@gmail.com>
2013-02-27 15:35:14 +0000

    Add cross compile configuration

---
+++ Source/robot/env/set_me_up.sh
@@ -0,0 +1,4 @@
+# Sets up the cross compile environment.
+wget http://www.codesourcery.com/sgpp/lite/arm/portal/package8739/public/arm-none-linux-gnueabi/arm-2011.03-41-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2
+tar xvf arm-2011.03-41-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2
+rm arm-2011.03-41-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2
