d82316175086d9794a3c9e822c794127b98b140f
Ken.Peersman@gmail.com <Ken.Peersman@gmail.com@1567ca2d-1607-d5b9-1ed5-aef1da3578f5>
2013-02-28 16:08:55 +0000

    eerste functies low level

--- trunk/Source/robot/low_level/low_level.h
+++ trunk/Source/robot/low_level/low_level.h
@@ -1,7 +1,12 @@
+/* Header low level communication
+ * @author Jasper
+ * @autor Ken
+*/
+
 #ifndef LOW_LEVEL_H
 #define LOW_LEVEL_H
 
-#define MAX_SPEED 250
+#define MAX_SPEED 200
 
 class LowLevel
 {
@@ -10,14 +15,42 @@ class LowLevel
         ~LowLevel();
         
     protected:
+        /* Function to drive the robot forward
+         * @param speed integer of speed in percent
+         */
         void driveForward(int percentSpeed);
+        /* Function to drive the robot backwards 
+         * @param speed integer of speed in percent
+         */
         void driveBackwards(int percentSpeed);
+        /* Function to rotate the robot to the left 
+         * @param speed integer of speed in percent
+         */
         void turnLeft(int percentSpeed);
+        /* Function to rotate the robot to the right
+         * @param speed integer of speed in percent
+         */
         void turnRight(int percentSpeed);
-        void stopMoving(void);
-         
+        /* Function to stop al movement of the robot
+         * @param speed integer of speed in percent
+         */
+        void stopMoving(void);         
+        /* Function to test the leds on the robot
+         */
+        void ledTest(void);
+
     private:
+        /* Function to calculate the value of percent
+         * @param integer that represent the percent
+         * @param integer that represent the maximum
+         */
         float percentToValue(int percent, int max);
-        void drive(bool richting, int speed);
+        /* Function to send command to the robot
+         * @param interger with the actuel speed
+         * @param character of the direction
+         */
+        void drive(int speed, char richting);
+
+        Serial * serial;
 }
 #endif //LOW_LEVEL_H
