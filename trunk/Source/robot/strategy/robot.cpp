d95d492e986ddd297a42f2dcfdd9fe2876810336
roelgooris@gmail.com <roelgooris@gmail.com@1567ca2d-1607-d5b9-1ed5-aef1da3578f5>
2013-03-01 13:55:57 +0000

    - added the class of the keeper - modified the abstract class of the robot

---
+++ trunk/Source/robot/strategy/robot.cpp
@@ -0,0 +1,20 @@
+#include "robot.h"
+
+using namespace std;
+
+    ///@fn  Robot  constructor
+    Robot::Robot()
+    {
+        ll = new LowLevel();
+        co = new Coords();
+        lastInstruction = '';
+    }     //ctor
+
+    ///@fn  ~Robot destructor
+    Robot::~Robot()
+    {
+        //verwijder ll en co
+    }    //dtor
+
+    virtual void run() = 0;
+    virtual void init();
