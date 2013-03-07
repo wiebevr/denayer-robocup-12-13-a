e0ec555691f3d96e63e10cab6a3cb969e60e3ce5
roelgooris@gmail.com <roelgooris@gmail.com@1567ca2d-1607-d5b9-1ed5-aef1da3578f5>
2013-03-07 19:30:28 +0000

    - implementing the changes of the vision departement - translating and re-organising the comments

--- trunk/Source/robot/strategy/keeper.cpp
+++ trunk/Source/robot/strategy/keeper.cpp
@@ -4,109 +4,94 @@
 
     Onderstaande code geschreven door Roel Gooris
 */
-
-
 /*
     idee om marge toe te voegen (positie is nooit exact):
     afronden van de binnengekregen coördinaten
 */
-
 
 #include "keeper.h"
 
 using namespace std;
 
     ///@fn  Keeper  constructor
-    Keeper::Keeper() 
+    Keeper::Keeper()
     {
-        initOK = false;
-        maxSpeed = 100;
+        initOK = false;     //the robot first has to go to the initialisation position! The function 'run' will do this when 'initOK' = false
+        maxSpeed = 100;     //100% -> move the robot at maximum speed
     }     //ctor
-
+//-------------------------------------------------------------------------------------------------------------------------------------------------------
     ///@fn  ~Keeper destructor
     Keeper::~Keeper() {}    //dtor
-
-    ///@fn  init    maak de robot klaar om te keeperen
-    void Keeper::init() {}   //de robot moet eerst naar zijn plaats rijden! De run functie doet dit als 'initOK' = false
-
-    ///@fn  run Laat de robot keeperen. !! Indien de robot nog niet op zijn initplaats staat, laat hem dan naar zijn initplaats gaan.
+//-------------------------------------------------------------------------------------------------------------------------------------------------------
+    ///@fn  run Let the robot keep. !! When the robot hasn't gone to the init place yet, he has to do this first (= initialisation)!!
     void Keeper::run()
     {
-        if (initOK)                         //als de robot al aan de goal staat kan er gekeeperd wordern
-            goKeeping();                   //achtervolg de bal (keeperen)
-        else                                //als de robot nog ergens anders staat, moet er nog eerst naar de goal worden gegaan
-            initOK = goToInitPlace();          //ga naar de goal en laat weten of we er zijn geraakt
+        if (initOK)                         //when the robot has been initialised, he can go keeping
+            goKeeping();                        //persuit the ball (keeping)
+        else                                //when the robot is not initialised yet, he has to do this first
+            initOK = goToInitPlace();           //this means he has to go to the initialisation place (his goal). Save the status of the init.
     }
-
-    ///@fn  goKeeping   Kijk of de bal op dit moment boven of onder de robot is (volgens de Y-as), en volg de bal.
+//-------------------------------------------------------------------------------------------------------------------------------------------------------
+    ///@fn  goKeeping   Simply look if the ball is under of above the robot (according the Y-axis), and follow the ball.
     void Keeper::goKeeping()
     {
-        //de coordinaten zitten in de hogere klasse, dus niet meer ophalen
-        if (co.bally > co.robot1y)     //is de bal boven de robot?
-        {
-            ll->driveForward(maxSpeed);                                 //ga naar boven (rij vooruit)
-        }
-        else                                               //de bal is onder de robot
-        {
-            ll->driveBackwards(maxSpeed);                                //ga naar onder (rij achteruit)
-        }
+        //the coordinates are available in the class 'co', which is already implemented by the 'strategy' class
+        if (co.bally > co.robot1y)          //is the ball above the robot?
+            ll->driveForward(maxSpeed);         //go up (drive forward)
+        else                                //the ball is below the robot
+            ll->driveBackwards(maxSpeed);       //go down (drive backwards)
     }
-
+//-------------------------------------------------------------------------------------------------------------------------------------------------------
     /**
-    * @fn   goToPlace   Initialiseer de keeper: rij naar de startplaats (in rechte lijnen, alleen horizontaal en verticaal).
-    * Er moet telkens nagegaan worden hoe ver de robot gevorderd is op dat moment: wordt in een lus aangeroepen met telkens een update van de coordinaten.
-    * De teruggegeven boolean duid aan of de initialisatie is voltooid (robot staat voor de goal).
+    * @fn   goToPlace   Initialise the keeper: drive to the init position (in straight lines, only horizontal en vertical).
+    * Every time this function is called, the progress of the initialisation has to be checked:
+    * this function is called in a loop, every time with an update of the coordinates.
+    * The returned boolean marks the status of the initialisation (robot stands (not yet) at the goal).
     */
-    ///@return  Indien = 'true': de robot staat op de goede plaats (en in de goede richting). Indien = 'false': de robot staat nog niet goed.
+    ///@return  When 'true': the robot has reached the init place (and is turned to the right direction). When'false': not there yet.
     bool Keeper::goToInitPlace()
     {
-        //staat de robot op de beginplaats?
-        if (co.robot1x == co.goal1x and co.robot1y == co.goal1y)
+        if (co.robot1x == co.goal1x and co.robot1y == co.goal1y)    //robot reached the init place?
         {
-            //staat de robot in de goede richting?
-            if (co.robotDir() == 90)
-	    {
-		ll->driveForward(0);
-                return true;  
-	    }                                              //de robot staat helemaal goed => stoppen
+            if (co.robot1roty == 1)                                     //... and turned towards the top?
+            {
+                ll->driveForward(0);                                        //stand still; stop moving
+                return true;                                                //init complete
+            }
             else
-            {                                                      //nog draaien zodat we naar boven kijken
-                if (co.robotDir() < 90 and co.robotDir() > -90)     //indien de robot naar rechts kijkt
-                    ll->turnLeft(maxSpeed);                                        //links draaien
-                else                                                        //indien de robot naar links kijkt
-                    ll->turnRight(maxSpeed);                                       //rechts draaien
+            {                                                           //... and not yet turned towards the top
+                if (co.robot1rotx > 0)                                      //when the robot looks to the right
+                    ll->turnLeft(maxSpeed);                                     //turn left
+                else                                                        //when the robot looks to the left
+                    ll->turnRight(maxSpeed);                                    //turn right
             }
         }
-        else
-        {                                                      //nog niet op de goede plaats
-            //staat de robot op dezelfde hoogte (Y-co) als zijn doel?
-            if (co.robot1y == co.goal1y)
+        else                                                        //robot has'n reached the init place yet
+        {
+            if (co.robot1y == co.goal1y)                                //But does he stands on the same vertical position? (y-axis)
             {
-                //staat de robot in de goede richting?
-                if (co.robotDir() == 180)
-                    ll->driveForward(maxSpeed);                                         //rechtdoor rijden
+                if (co.robot1rotx == -1)                                     //... and is he turned towards the goal?
+                    ll->driveForward(maxSpeed);                                 //then go forward
                 else
-                {                                                      //nog draaien zodat we naar links kijken
-                    if (co.robotDir() < 180 and co.robotDir())     //indien de robot naar boven kijkt
-                        ll->turnLeft(maxSpeed);                                        //links draaien
-                    else                                                        //indien de robot naar onder kijkt
-                        ll->turnRight(maxSpeed);                                       //rechts draaien
+                {                                                           //... and not yet turned towards the goal
+                    if (co.robot1roty > 0)                                      //when the robot looks to the top
+                        ll->turnLeft(maxSpeed);                                     //turn left
+                    else                                                        //when the robot looks to the bottom
+                        ll->turnRight(maxSpeed);                                    //turn right
                 }
             }
-            //de robot staat volledig uit positie
-            else
+            else                                                        //When the robot is completely out of position
             {
-                //staat de robot in de goede richting?
-                if (co.robotDir() == 90)
-                    ll->driveForward(maxSpeed);                                          //rechtdoor rijden
-                else
-                {                                                      //nog draaien zodat we naar boven kijken
-                    if (co.robotDir() < 90 and co.robotDir() > -90)     //indien de robot naar rechts kijkt
-                        ll->turnLeft(maxSpeed);                                        //links draaien
-                    else                                                        //indien de robot naar links kijkt
-                        ll->turnRight(maxSpeed);                                       //rechts draaien
+                if (co.robot1roty == 1)                                     //... and the robot is turned towards the top
+                    ll->driveForward(maxSpeed);                                 //then go forward
+                else                                                        //... and the robot is neither turned towards the top
+                {
+                    if (co.robot1rotx > 0)                                      //when the robot looks to the right
+                        ll->turnLeft(maxSpeed);                                     //turn left
+                    else                                                        //when the robot looks to the left
+                        ll->turnRight(maxSpeed);                                    //turn right
                 }
             }
         }
-        return false;                                               //indien we tot hier raken dan staat de robot nog niet op de goede plaats
+        return false;                                               //when the init is not terminated yet, then the initialisation is not completed yet
     }
