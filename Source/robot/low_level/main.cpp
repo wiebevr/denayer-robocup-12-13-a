cfd667599b5b619c7b186198862eeab40309f880
vercammenjasper <vercammenjasper@gmail.com>
2013-03-27 19:50:27 +0000

    Made a makefile for testing the serial class and low level class withous cross-compiling

--- Source/robot/low_level/main.cpp
+++ Source/robot/low_level/main.cpp
@@ -1,26 +1,27 @@
 // Includes:
-#include "ipc/ipc_server.h"
+//#include "ipc/ipc_server.h"
 
 #include "low_level.h"
 
 // Standard:
 #include <iostream>
+#include <stdlib.h>
 
 // ---------------------------------------------------------------------------
 int main(int argc, char **argv)
 {
-    IPCServer strategy(IPCSocket::WIFI_STRATEGY);
-    strategy.open();
+//    IPCServer strategy(IPCSocket::WIFI_STRATEGY);
+//    strategy.open();
     
-    char buffer[IPCSocket::MAX_MESSAGE_LEN];
+//    char buffer[IPCSocket::MAX_MESSAGE_LEN];
     int length;
     LowLevel * ll = new LowLevel();
-    ll->driveForward(100);
+    ll->driveForward(10);
     for( int i = 0; i < 100000; i++) {}
     ll->stopMoving();
     while (true)
     {
-        std::cout << strategy << std::endl;
+        std::cout << "Niets doen!" << std::endl;
     }
 
     return 0;
