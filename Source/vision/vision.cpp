db53bff23e9e74a7dac7eb7319fcc822f08386f7
raphael.den.dooven <raphael.den.dooven@gmail.com>
2013-04-25 13:24:52 +0000

    Bug fix

--- Source/vision/vision.cpp
+++ Source/vision/vision.cpp
@@ -42,7 +42,7 @@ Mat Vision::initCamera(const string& filename)
         exit(EXIT_FAILURE);
     }
     cap >> img;
-	//cvtColor(img, img, CV_BayerBG2BGR);
+	cvtColor(img, img, CV_BayerBG2BGR);
 		//img = imread("../buildHome/Img/Keeper.ppm");
 		
     return img;
@@ -99,7 +99,7 @@ VisionData &Vision::fetchData()
 
     // Get new camera frame
     cap >> img;
-	//cvtColor(img, img, CV_BayerBG2BGR);
+	cvtColor(img, img, CV_BayerBG2BGR);
 		//img = imread("../buildHome/Img/Keeper.ppm");
 
     // Get transformed image and put it in tmp image
