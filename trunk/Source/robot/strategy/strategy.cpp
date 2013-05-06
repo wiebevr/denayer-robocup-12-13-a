6b46ec20fe9e695bbd699a7c8f55f5a4f5d2309e
gertmertes <gertmertes@1567ca2d-1607-d5b9-1ed5-aef1da3578f5>
2013-05-06 12:50:21 +0000

    trololo

--- trunk/Source/robot/strategy/strategy.cpp
+++ trunk/Source/robot/strategy/strategy.cpp
@@ -47,6 +47,23 @@ void Strategy::run()
 void Strategy::setCoords(VisionData *coords)
 {
 //    this->co = game;
+	coords->robot1x = round((coords->robot1x * 100));
+	coords->robot1y = round((coords->robot1y * 100));
+	coords->robot1rotx = round((coords->robot1rotx * 100));
+	coords->robot1roty = round((coords->robot1roty * 100));
+	coords->robot2x = round((coords->robot2x * 100));
+	coords->robot2y = round((coords->robot2y * 100));
+	coords->robot2rotx = round((coords->robot2rotx * 100));
+	coords->robot2roty = round((coords->robot2roty * 100));
+	
+	coords->ballx = round((coords->ballx * 100));
+	coords->bally = round((coords->bally * 100));
+
+	coords->goal1x = round((coords->goal1x * 100));
+	coords->goal1y = round((coords->goal1y * 100));
+	coords->goal2x = round((coords->goal2x * 100));
+	coords->goal2y = round((coords->goal2y * 100));
+
 	keeper->setCoords(coords);
 	kicker->setCoords(coords);	
 }
